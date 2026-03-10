4728a5539ae2896825aa907ed5e7d562ef1aa2f3
zenst45 <noenabon41@gmail.com>
2026-03-10 20:44:56 +0100

    Revert "app.py: host"
    This reverts commit e91757689a1eb202a78a467be36bbfd4304a0895.

--- app.py
+++ app.py
@@ -845,4 +845,4 @@ def delete_archive(filename):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(debug=True, port=5000)
